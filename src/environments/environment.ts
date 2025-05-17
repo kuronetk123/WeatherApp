48950d5bbb29d653a1256064b788360e3411e507
kuronetk123 <phongltt0203@gmail.com>
2025-05-17 21:19:34 +0700

    Add environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'x-rapidapi-host',
+  XRapidAPIHostHeaderValue: 'weather-api167.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'x-rapidapi-key',
+    XRapidAPIKeyHeaderValue: 'b3b586a424msh1014cb4c7d661bep1b21bcjsn7d31f154c823',
+};
