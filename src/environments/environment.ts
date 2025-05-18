cac27f843300d90ca0c70d11ab9ad682f812aff2
kuronetk123 <phongltt0203@gmail.com>
2025-05-18 14:32:23 +0700

    DeleteAPINotNeed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
   weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
-  XRapidAPIHostHeaderName: 'x-rapidapi-host',
-  XRapidAPIHostHeaderValue: 'weather-api167.p.rapidapi.com',
-    XRapidAPIKeyHeaderName: 'x-rapidapi-key',
-    XRapidAPIKeyHeaderValue: 'b3b586a424msh1014cb4c7d661bep1b21bcjsn7d31f154c823',
 };
